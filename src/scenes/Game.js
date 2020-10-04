e1719f40cc475f1858682913f118af439964f410
Meredith Mello <merry@mello.codes>
2020-10-04 16:07:44 -0700

    Added more stuff from tutorial, keyboard input

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,8 +1,14 @@
 import Phaser from '../lib/phaser.js'
 
 export default class Game extends Phaser.Scene {
+  /** @type {Phaser.Physics.Arcade.StaticGroup} */
+  platforms
+  
   /** @type {Phaser.Physics.Arcade.Sprite} */
   player
+  
+  /** @type {Phaser.Types.Input.Keyboard.CursorKeys} */
+  cursors
 
   constructor() {
     super('game')
@@ -13,33 +19,53 @@ export default class Game extends Phaser.Scene {
     this.load.image('platform', 'assets/ground_grass.png')
 
     this.load.image('bunny-stand', 'assets/bunny1_stand.png')
+    
+    this.cursors = this.input.keyboard.createCursorKeys()
   }
 
   create() {
     this.add.image(240, 320, 'background')
+      .setScrollFactor(1, 0)
 
-    const platforms = this.physics.add.staticGroup()
+    this.platforms = this.physics.add.staticGroup()
 
     for (let i = 0; i < 5; ++i) {
       const x = Phaser.Math.Between(80, 400)
       const y = 150 * i
 
       /** @type {Phaser.Physics.Arcade.Sprite} */
-      const platform = platforms.create(x, y, 'platform');
+      const platform = this.platforms.create(x, y, 'platform');
       platform.scale = 0.5
 
       /** @type {Phaser.Physics.Arcade.StaticBody} */
       const body = platform.body
       body.updateFromGameObject()
-
-      this.player = this.physics.add.sprite(240, 320, 'bunny-stand')
+    }
+    
+    this.player = this.physics.add.sprite(240, 320, 'bunny-stand')
         .setScale(0.5)
 
-      this.physics.add.collider(platforms, this.player)
-    }
+    this.physics.add.collider(this.platforms, this.player)
+      
+    this.player.body.checkCollision.up = false
+    this.player.body.checkCollision.left = false
+    this.player.body.checkCollision.right = false
+      
+    this.cameras.main.startFollow(this.player)
   }
 
   update() {
+    this.platforms.children.iterate(child => {
+      /** @type {Phaser.Physics.Arcade.Sprite} */
+      const platform = child
+      
+      const scrollY = this.cameras.main.scrollY
+      if (platform.y >= scrollY + 700) {
+        platform.y = scrollY - Phaser.Math.Between(50, 100)
+        platform.body.updateFromGameObject()
+      }
+    })
+    
     const touchingDown = this.player.body.touching.down
 
     if (touchingDown) {
